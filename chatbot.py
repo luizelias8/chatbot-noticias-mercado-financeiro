c094915e7fd40897a52c51a76cb62d9e17d8e4f3
luizelias8 <luizelias8@gmail.com>
2024-11-20 09:28:25 -0300

    Refatora código para substituir o uso de Langchain pela biblioteca da Groq

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-from langchain_groq import ChatGroq
+from groq import Groq
 from dotenv  import load_dotenv
 import os
 from scraping import raspar_noticias # Importa a função de raspagem de notícias
@@ -6,10 +6,8 @@ from scraping import raspar_noticias # Importa a função de raspagem de notíci
 # Carrega as variáveis de ambiente do arquivo .env, incluindo a chave da API do OpenAI.
 load_dotenv()
 
-# Configura o modelo de linguagem
-cliente = ChatGroq(
-    model='llama-3.1-70b-versatile',
-    temperature=0, # Controla a criatividade do modelo
+# Configura o cliente do modelo de linguagem
+cliente = Groq(
     api_key=os.getenv('GROQ_API_KEY')
 )
 
@@ -36,13 +34,17 @@ while True:
         break
 
     # Adiciona a mensagem do usuário no histórico
-    mensagens.append({'role': 'human', 'content': entrada_usuario})
+    mensagens.append({'role': 'user', 'content': entrada_usuario})
 
     # Gera uma resposta usando o modelo Llama
-    resposta = cliente.invoke(mensagens)
+    resposta = cliente.chat.completions.create(
+        messages=mensagens,
+        temperature=0, # Controla a criatividade do modelo
+        model='llama-3.1-70b-versatile'
+    )
 
     # Adiciona a resposta da IA no histórico
-    mensagens.append({'role': 'ai', 'content': resposta.content})
+    mensagens.append({'role': 'assistant', 'content': resposta.choices[0].message.content})
 
     # Exibe a resposta no terminal
-    print(f"Assistente: {resposta.content}")
+    print(f"Assistente: {resposta.choices[0].message.content}")
